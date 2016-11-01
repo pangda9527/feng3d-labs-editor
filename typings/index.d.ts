f607fa9f55714a85f54767f78dbabb24651f467b
feng <908087098@qq.com>
2016-11-01 09:34:00 +0800

    更新 feng3d

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../feng3d/release/feng3d.d.ts" />
+/// <reference path="../feng3d/out/feng3d.d.ts" />
 /// <reference path="LayaAir.d.ts" />
 /// <reference path="globals/github-electron/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
