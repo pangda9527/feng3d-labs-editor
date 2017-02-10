f29c5b56179ce6d034b2ab45e405f4196198d211
feng <908087098@qq.com>
2017-02-10 17:46:27 +0800

    加入 objectView 到 editor

--- src/Editor.ts
+++ src/Editor.ts
@@ -11,13 +11,21 @@ module feng3d.editor {
             //初始化feng3d
             new Main3D();
             //初始化界面
-            Laya.init(600, 400);
+            Laya.init(Laya.Browser.width, Laya.Browser.height);
             Laya.loader.load([{ url: "res/atlas/comp.json", type: laya.net.Loader.ATLAS }], laya.utils.Handler.create(null, onLoaded));
 
             function onLoaded(): void {
                 //实例UI界面
-                var testUI = new MainUI();
-                Laya.stage.addChild(testUI);
+                // var testUI = new MainUI();
+                // Laya.stage.addChild(testUI);
+
+                //背景完全透明
+                Laya.stage.bgColor = "0";
+
+                var view = ObjectView.getObjectView(new Vector3D());
+                view.x = 20;
+                view.y = 20;
+                Laya.stage.addChild(view);
             }
         }
     }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
+/// <reference path="LayaAir.d.ts" />
 /// <reference path="../feng3d/out/feng3d.d.ts" />
 /// <reference path="../feng3d/typings/index.d.ts" />
-/// <reference path="LayaAir.d.ts" />
+/// <reference path="../objectview/out/objectview.d.ts" />
 /// <reference path="globals/github-electron/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
