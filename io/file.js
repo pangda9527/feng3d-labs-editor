634518c6f4bf5031b35339b09fa682b28ae7ff8d
feng <908087098@qq.com>
2017-12-22 15:12:11 +0800

    补充提交文件

---
+++ io/file.js
@@ -0,0 +1,170 @@
+"use strict";
+
+var fs = require("fs-extra");
+var process = require('child_process');
+var path = require("path");
+
+/**
+ * 工作空间路径，工作空间内存放所有编辑器项目
+ */
+var workspace = "e:/editorworkspace";
+
+/**
+ * 项目路径
+ */
+var projectpath;
+
+exports.file = {
+    createproject: function (projectname, callback)
+    {
+        var templatepath = path.join(__dirname, "../templates/template");
+        var projectpath = workspace + "/" + projectname;
+        if (fs.existsSync(projectpath))
+        {
+            fs.copy(templatepath, workspace + "/" + projectname, { overwrite: true }, callback);
+        } else
+        {
+            fs.mkdir(projectpath, (err) =>
+            {
+                fs.copy(templatepath, workspace + "/" + projectname, { overwrite: true }, callback);
+            });
+        }
+    },
+    initproject: function (projectname, callback)
+    {
+        console.log(`exec tsc -w -p ${projectname}`)
+        var childProcess = process.exec('tsc -w -p ' + workspace + "/" + projectname, function (error, stdout, stderr)
+        {
+            if (error !== null)
+            {
+                console.log('exec error: ' + error);
+            }
+            console.log(stdout)
+            console.log(stderr)
+        });
+        childProcess.stdout.on('data', function (data)
+        {
+            data = data.trim();
+            console.log(data);
+        });
+        childProcess.stderr.on('data', function (data)
+        {
+            data = data.trim();
+            console.error(data);
+        });
+        projectpath = workspace + "/" + projectname;
+        callback();
+    },
+    stat: function (path, callback)
+    {
+        var realpath = getProjectPath(path);
+        fs.stat(realpath, (err, stats) =>
+        {
+            if (err)
+            {
+                callback(err, null);
+                return;
+            }
+            var fileInfo = {
+                path: path,
+                size: stats.size,
+                isDirectory: stats.isDirectory(),
+                birthtime: stats.birthtime.getTime(),
+                mtime: stats.mtime.getTime(),
+                children: []
+            }
+            callback(err, fileInfo);
+        });
+    },
+    readdir: function (path, callback)
+    {
+        path = getProjectPath(path);
+        fs.readdir(path, callback);
+    },
+    writeFile: function (path, data, callback)
+    {
+        path = getProjectPath(path);
+        fs.open(path, "w", (err, fd) =>
+        {
+            if (err)
+            {
+                callback && callback(err);
+                return;
+            }
+            fs.writeFile(path, data, (err) =>
+            {
+                fs.close(fd);
+                callback && callback(err);
+            });
+        });
+    }
+    ,
+    readFile: function (path, callback)
+    {
+        path = getProjectPath(path);
+        fs.open(path, "r", (err, fd) =>
+        {
+            if (err)
+            {
+                callback(err, null);
+                return;
+            }
+            fs.readFile(path, "utf8", (err, data) =>
+            {
+                fs.close(fd);
+                callback(err, data);
+            });
+        });
+    },
+    remove: function (path, callback)
+    {
+        path = getProjectPath(path);
+        fs.remove(path, callback);
+    },
+    mkdir: function (path, callback)
+    {
+        path = getProjectPath(path);
+        fs.mkdir(path, callback);
+    },
+    rename: function (oldPath, newPath, callback)
+    {
+        oldPath = getProjectPath(oldPath);
+        newPath = getProjectPath(newPath);
+        fs.rename(oldPath, newPath, callback);
+    },
+    move: function (src, dest, callback)
+    {
+        src = getProjectPath(src);
+        dest = getProjectPath(dest);
+        fs.move(src, dest, callback);
+    },
+    /**
+     * 获取项目列表
+     */
+    getProjectList: function (callback)
+    {
+        fs.readdir(workspace, (err, files) =>
+        {
+            callback(err, files);
+        });
+    },
+    /**
+     * 判断指定项目是否存在
+     */
+    hasProject: function (projectname, callback)
+    {
+        fs.stat(workspace + "/" + projectname, (err, stats) =>
+        {
+            callback(!err);
+        });
+    }
+};
+
+function getProjectPath(path)
+{
+    if (!projectpath)
+        return path;
+    if (path.indexOf(projectpath) != -1)
+        return path;
+    return projectpath + "/" + path;
+}
