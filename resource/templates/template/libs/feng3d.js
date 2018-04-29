904ab62fcbdb0d5b50d8e28a6bd744d4087f5a84
feng <908087098@qq.com>
2018-04-29 21:38:46 +0800

    更新 feng3d

--- resource/templates/template/libs/feng3d.js
+++ resource/templates/template/libs/feng3d.js
@@ -10776,7 +10776,7 @@ var feng3d;
         ], TextureInfo.prototype, "format", null);
         __decorate([
             feng3d.serialize(feng3d.TextureDataType.UNSIGNED_BYTE),
-            feng3d.oav()
+            feng3d.oav({ component: "OAVEnum", componentParam: { enumClass: feng3d.TextureDataType } })
         ], TextureInfo.prototype, "type", null);
         __decorate([
             feng3d.serialize(false),
@@ -10792,19 +10792,19 @@ var feng3d;
         ], TextureInfo.prototype, "premulAlpha", null);
         __decorate([
             feng3d.serialize(feng3d.TextureMinFilter.LINEAR),
-            feng3d.oav()
+            feng3d.oav({ component: "OAVEnum", componentParam: { enumClass: feng3d.TextureMinFilter } })
         ], TextureInfo.prototype, "minFilter", void 0);
         __decorate([
             feng3d.serialize(feng3d.TextureMagFilter.LINEAR),
-            feng3d.oav()
+            feng3d.oav({ component: "OAVEnum", componentParam: { enumClass: feng3d.TextureMagFilter } })
         ], TextureInfo.prototype, "magFilter", void 0);
         __decorate([
             feng3d.serialize(feng3d.TextureWrap.REPEAT),
-            feng3d.oav()
+            feng3d.oav({ component: "OAVEnum", componentParam: { enumClass: feng3d.TextureWrap } })
         ], TextureInfo.prototype, "wrapS", void 0);
         __decorate([
             feng3d.serialize(feng3d.TextureWrap.REPEAT),
-            feng3d.oav()
+            feng3d.oav({ component: "OAVEnum", componentParam: { enumClass: feng3d.TextureWrap } })
         ], TextureInfo.prototype, "wrapT", void 0);
         __decorate([
             feng3d.serialize(0),
