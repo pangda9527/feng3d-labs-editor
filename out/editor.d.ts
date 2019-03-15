6268ae99d6e6b7d634bd3795c5a6272b55903275
feng <908087098@qq.com>
2019-03-16 01:42:57 +0800

    扩展 统一放到polyfill文件夹中

--- out/editor.d.ts
+++ out/editor.d.ts
@@ -1,4 +1,64 @@
 /// <reference path="../libs/typescriptServices.d.ts" />
+declare namespace feng3d {
+}
+declare namespace feng3d {
+    /**
+     * (快捷键)状态列表
+     */
+    var shortCutStates: {
+        disableScroll: string;
+    };
+    /**
+     * (快捷键)状态列表
+     */
+    type ShortCutStates = typeof shortCutStates;
+    interface ShortCut {
+        activityState<K extends keyof ShortCutStates>(state: K): void;
+        deactivityState<K extends keyof ShortCutStates>(state: K): void;
+        getState<K extends keyof ShortCutStates>(state: K): boolean;
+    }
+}
+declare namespace egret {
+    /**
+     * 扩展鼠标事件，增加鼠标 按下、弹起、移动、点击、移入、移出、右击、双击事件
+     */
+    type MouseEvent = egret.TouchEvent;
+    var MouseEvent: {
+        prototype: TouchEvent;
+        new (): TouchEvent;
+        /** 鼠标按下 */
+        MOUSE_DOWN: "mousedown";
+        /** 鼠标弹起 */
+        MOUSE_UP: "mouseup";
+        /** 鼠标移动 */
+        MOUSE_MOVE: "mousemove";
+        /** 鼠标单击 */
+        CLICK: "click";
+        /** 鼠标中键按下 */
+        MIDDLE_MOUSE_DOWN: "middlemousedown";
+        /** 鼠标中键弹起 */
+        MIDDLE_MOUSE_UP: "middlemouseup";
+        /** 鼠标中键点击 */
+        MIDDLE_Click: "middleclick";
+        /** 鼠标移出 */
+        MOUSE_OUT: "mouseout";
+        /** 鼠标移入 */
+        MOUSE_OVER: "mouseover";
+        /** 右键按下 */
+        RIGHT_MOUSE_DOWN: "rightmousedown";
+        /** 鼠标右键弹起 */
+        RIGHT_MOUSE_UP: "rightmouseup";
+        /** 右键点击 */
+        RIGHT_CLICK: "rightclick";
+        /** 双击 */
+        DOUBLE_CLICK: "dblclick";
+    };
+    var mouseEventEnvironment: () => void;
+}
+declare namespace egret {
+}
+declare namespace egret {
+}
 declare namespace editor {
     var giteeOauth: GiteeOauth;
     /**
@@ -481,23 +541,6 @@ declare namespace editor {
          * 立方体纹理
          */
         texturecube?: feng3d.TextureCube;
-    }
-}
-declare namespace feng3d {
-    /**
-     * (快捷键)状态列表
-     */
-    var shortCutStates: {
-        disableScroll: string;
-    };
-    /**
-     * (快捷键)状态列表
-     */
-    type ShortCutStates = typeof shortCutStates;
-    interface ShortCut {
-        activityState<K extends keyof ShortCutStates>(state: K): void;
-        deactivityState<K extends keyof ShortCutStates>(state: K): void;
-        getState<K extends keyof ShortCutStates>(state: K): boolean;
     }
 }
 declare namespace editor {
@@ -3342,42 +3385,6 @@ declare namespace editor {
         DontHeight = 2,
         IsContour = 4
     }
-}
-declare namespace egret {
-}
-declare namespace egret {
-    type MouseEvent = egret.TouchEvent;
-    var MouseEvent: {
-        prototype: TouchEvent;
-        new (): TouchEvent;
-        /** 鼠标按下 */
-        MOUSE_DOWN: "mousedown";
-        /** 鼠标弹起 */
-        MOUSE_UP: "mouseup";
-        /** 鼠标移动 */
-        MOUSE_MOVE: "mousemove";
-        /** 鼠标单击 */
-        CLICK: "click";
-        /** 鼠标中键按下 */
-        MIDDLE_MOUSE_DOWN: "middlemousedown";
-        /** 鼠标中键弹起 */
-        MIDDLE_MOUSE_UP: "middlemouseup";
-        /** 鼠标中键点击 */
-        MIDDLE_Click: "middleclick";
-        /** 鼠标移出 */
-        MOUSE_OUT: "mouseout";
-        /** 鼠标移入 */
-        MOUSE_OVER: "mouseover";
-        /** 右键按下 */
-        RIGHT_MOUSE_DOWN: "rightmousedown";
-        /** 鼠标右键弹起 */
-        RIGHT_MOUSE_UP: "rightmouseup";
-        /** 右键点击 */
-        RIGHT_CLICK: "rightclick";
-        /** 双击 */
-        DOUBLE_CLICK: "dblclick";
-    };
-    var mouseEventEnvironment: () => void;
 }
 declare namespace editor {
     /**
---
+++ src/polyfill/egret/Scroller.ts
@@ -0,0 +1,35 @@
+namespace egret
+{
+    // 扩展 Scroller 组件，添加鼠标滚轮事件
+    var oldOnAddToStage = eui.Scroller.prototype.$onAddToStage;
+    eui.Scroller.prototype.$onAddToStage = function (stage: egret.Stage, nestLevel: number): void
+    {
+        oldOnAddToStage.call(this, stage, nestLevel);
+        feng3d.windowEventProxy.on("wheel", onMouseWheel, this);
+    }
+    var oldOnRemoveFromStage = eui.Scroller.prototype.$onRemoveFromStage;
+    eui.Scroller.prototype.$onRemoveFromStage = function (): void
+    {
+        oldOnRemoveFromStage.call(this);
+        feng3d.windowEventProxy.off("wheel", onMouseWheel, this);
+    }
+
+    // 阻止拖拽滚动面板
+    var oldonTouchMove = eui.Scroller.prototype["onTouchMove"];
+    eui.Scroller.prototype["onTouchMove"] = function (event)
+    {
+        if (feng3d.shortcut.getState("disableScroll"))
+            return;
+        oldonTouchMove.call(this, event);
+    }
+
+    function onMouseWheel(event: WheelEvent)
+    {
+        var scroller: eui.Scroller = this;
+        if (scroller.hitTestPoint(feng3d.windowEventProxy.clientX, feng3d.windowEventProxy.clientY))
+        {
+            scroller.viewport.scrollV = feng3d.FMath.clamp(scroller.viewport.scrollV + event.deltaY * 0.3, 0, scroller.viewport.contentHeight - scroller.height);
+        }
+    }
+
+}
