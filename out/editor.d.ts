449f104838909eba2498112f1a08d7026c7bb23c
feng <908087098@qq.com>
2019-03-16 00:41:44 +0800

    处理 Scroller

--- out/editor.d.ts
+++ out/editor.d.ts
@@ -483,6 +483,23 @@ declare namespace editor {
         texturecube?: feng3d.TextureCube;
     }
 }
+declare namespace feng3d {
+    /**
+     * (快捷键)状态列表
+     */
+    var shortCutStates: {
+        disableScroll: string;
+    };
+    /**
+     * (快捷键)状态列表
+     */
+    type ShortCutStates = typeof shortCutStates;
+    interface ShortCut {
+        activityState<K extends keyof ShortCutStates>(state: K): void;
+        deactivityState<K extends keyof ShortCutStates>(state: K): void;
+        getState<K extends keyof ShortCutStates>(state: K): boolean;
+    }
+}
 declare namespace editor {
     class Editorshortcut {
         private preMousePoint;
