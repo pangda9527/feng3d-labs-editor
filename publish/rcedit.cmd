aac68d12b7c797e6d75bc823f3a83dc9a32df487
feng <908087098@qq.com>
2019-03-22 21:35:05 +0800

    删除 publish/electron

---
+++ publish/rcedit.cmd
@@ -0,0 +1,7 @@
+rcedit-x64.exe "electron/feng3d-editor.exe" --set-version-string "Comments" "This is an feng3d-editor"
+rcedit-x64.exe "electron/feng3d-editor.exe" --set-file-version "0.1"
+rcedit-x64.exe "electron/feng3d-editor.exe" --set-product-version "0.1"
+rcedit-x64.exe "electron/feng3d-editor.exe" --set-icon "favicon.ico"
+REM rcedit-x64.exe "electron/feng3d-editor.exe" --set-requested-execution-level "requireAdministrator"
+REM rcedit-x64.exe "electron/feng3d-editor.exe" --application-manifest "Manifests.xml"
+REM rcedit-x64.exe "electron/feng3d-editor.exe" --set-resource-string id_number "1018"
