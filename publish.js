163ad6b7b82aea98cbd52612d1b9a6d78e60a8ec
feng <908087098@qq.com>
2019-03-22 21:06:33 +0800

    调整 publish.js

--- publish.js
+++ publish.js
@@ -1,3 +1,4 @@
+var process = require('child_process');
 var fs = require("fs-extra");
 var path = require("path");
 var asar = require("asar");
@@ -7,16 +8,22 @@ var exclude = [".git", "node_modules", "electron", "src", "release", "publish"];
 var files = fs.readdirSync(__dirname);
 files = files.filter(f => exclude.indexOf(f) == -1);
 
+// 计算路径
+var releasePath = path.resolve(__dirname, "release");
+var appPath = path.resolve(__dirname, "node_modules/electron/dist/resources/app.asar");
+var electronExePath = path.resolve(__dirname, "node_modules/electron/dist/electron.exe");
+
 // 删除历史文件
-fs.removeSync(path.resolve(__dirname, "release"));
-fs.removeSync(path.resolve(__dirname, "publish/electron/resources/app.asar"));
+fs.removeSync(releasePath);
+fs.removeSync(appPath);
 
 // 拷贝文件
 files.forEach(f =>
 {
-    fs.copySync(path.resolve(__dirname, f), path.resolve(__dirname, "release", f));
+    fs.copySync(path.resolve(__dirname, f), path.resolve(releasePath, f));
 });
 
 // 打包 asar
-asar.createPackage(path.resolve(__dirname, "release"), path.resolve(__dirname, "publish/electron/resources/app.asar"));
+asar.createPackage(releasePath, appPath);
 
+console.log(`完成！`);
