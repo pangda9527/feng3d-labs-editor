ebf29b9245fb6cf371b6a29bff80f053ca630602
feng <908087098@qq.com>
2019-04-19 22:51:47 +0800

    整理 publish.js

--- publish.js
+++ publish.js
@@ -4,10 +4,48 @@ var fs = require("fs-extra");
 var path = require("path");
 var asar = require("asar");
 
-if (!fs.existsSync(path.resolve(__dirname, "publish")))
+downloadElectron(() =>
 {
+    exeRename(() =>
+    {
+        packageasar(() =>
+        {
+            console.log(`完成！`);
+        });
+    });
+});
+
+function downloadElectron(callback)
+{
+    if (!fs.existsSync(path.resolve(__dirname, "publish")))
+    {
+        // 下载 electron 包
+        var cmdstr = 'git.exe clone --progress -v "https://gitee.com/feng3d_admin/editor-publish-electron.git" "publish"';
+        console.log(cmdstr);
+        process.exec(cmdstr, function (error, stdout, stderr)
+        {
+            if (error !== null)
+            {
+                console.log('exec error: ' + error);
+            }
+            console.log(stdout)
+            console.log(stderr)
+            callback();
+        });
+    } else
+    {
+        callback()
+    }
+}
+
+function exeRename(callback)
+{
+    if (!fs.existsSync(path.resolve(__dirname, "publish/electron/feng3d-editor.exe")))
+    {
+        fs.moveSync(path.resolve(__dirname, "publish/electron/electron.exe"), path.resolve(__dirname, "publish/electron/feng3d-editor.exe"))
+    }
     // 下载 electron 包
-    var cmdstr = 'git.exe clone --progress -v "https://gitee.com/feng3d_admin/editor-publish-electron.git" "publish"';
+    var cmdstr = path.resolve(__dirname, 'publish/rcedit.cmd');
     console.log(cmdstr);
     process.exec(cmdstr, function (error, stdout, stderr)
     {
@@ -17,15 +55,14 @@ if (!fs.existsSync(path.resolve(__dirname, "publish")))
         }
         console.log(stdout)
         console.log(stderr)
-        packageasar();
+        callback();
     });
-} else
-{
-    packageasar()
 }
 
-function packageasar()
+function packageasar(callback)
 {
+    console.log(`打包 asar 文件中...`);
+
     // 文件列表
     var files = [
         './node_modules/feng3d/out',
@@ -51,7 +88,6 @@ function packageasar()
     // 计算路径
     var releasePath = path.resolve(__dirname, "release");
     var appPath = path.resolve(__dirname, "publish/electron/resources/app.asar");
-    var electronExePath = path.resolve(__dirname, "node_modules/electron/dist/electron.exe");
 
     // 删除历史文件
     fs.removeSync(appPath);
@@ -67,7 +103,6 @@ function packageasar()
     {
         // 删除临时文件
         fs.removeSync(releasePath);
+        callback();
     });
-
-    console.log(`完成！`);
 }
