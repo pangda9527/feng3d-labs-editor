9ea73e12f9a94dfe0396af5ad40940bfd1e7953d
feng <908087098@qq.com>
2019-12-12 19:01:08 +0800

    load.ts

--- out/editor.d.ts
+++ out/editor.d.ts
@@ -4343,6 +4343,34 @@ declare namespace editor {
         private onMousedown;
     }
 }
+declare namespace feng3d {
+    var loadjs: {
+        load: (params: {
+            paths: string | string[] | {
+                url: string;
+                type: string;
+            } | {
+                url: string;
+                type: string;
+            }[];
+            bundleId?: string;
+            success?: () => void;
+            error?: (pathsNotFound?: string[]) => void;
+            async?: boolean;
+            numRetries?: number;
+            before?: (path: {
+                url: string;
+                type: string;
+            }, e: any) => boolean;
+            onitemload?: (url: string, content: string) => void;
+        }) => void;
+        ready: (params: {
+            depends: string | string[];
+            success?: () => void;
+            error?: (pathsNotFound?: string[]) => void;
+        }) => void;
+    };
+}
 declare namespace editor {
     class ThreejsLoader {
         load(url: string, completed?: (gameobject: feng3d.GameObject) => void): void;
