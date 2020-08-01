094cf6c8f54832c6c726e76cb20f654ea779b736
feng <908087098@qq.com>
2020-08-01 12:12:44 +0800

    调整 OAVVector4

--- src/objectview/oav/OAVVector4.ts
+++ src/objectview/oav/OAVVector4.ts
@@ -17,6 +17,31 @@ namespace editor
         public wLabel: eui.Label;
         public wTextInput: eui.TextInput;
 
+        /**
+		 * 步长，精度
+		 */
+        step = 0.001;
+
+		/**
+		 * 键盘上下方向键步长
+		 */
+        stepDownup = 0.001;
+
+		/**
+		 * 移动一个像素时增加的步长数量
+		 */
+        stepScale = 1;
+
+        /**
+         * 最小值
+         */
+        minValue = NaN;
+
+        /**
+         * 最小值
+         */
+        maxValue = NaN;
+
         constructor(attributeViewInfo: feng3d.AttributeViewInfo)
         {
             super(attributeViewInfo);
@@ -30,19 +55,19 @@ namespace editor
 
             this.addBinder(new NumberTextInputBinder().init({
                 space: this.attributeValue, attribute: "x", textInput: this.xTextInput, editable: this._attributeViewInfo.editable,
-                controller: this.xLabel,
+                controller: this.xLabel, step: this.step, stepDownup: this.stepDownup, stepScale: this.stepScale, minValue: this.minValue, maxValue: this.maxValue,
             }));
             this.addBinder(new NumberTextInputBinder().init({
                 space: this.attributeValue, attribute: "y", textInput: this.yTextInput, editable: this._attributeViewInfo.editable,
-                controller: this.yLabel,
+                controller: this.yLabel, step: this.step, stepDownup: this.stepDownup, stepScale: this.stepScale, minValue: this.minValue, maxValue: this.maxValue,
             }));
             this.addBinder(new NumberTextInputBinder().init({
                 space: this.attributeValue, attribute: "z", textInput: this.zTextInput, editable: this._attributeViewInfo.editable,
-                controller: this.zLabel,
+                controller: this.zLabel, step: this.step, stepDownup: this.stepDownup, stepScale: this.stepScale, minValue: this.minValue, maxValue: this.maxValue,
             }));
             this.addBinder(new NumberTextInputBinder().init({
                 space: this.attributeValue, attribute: "w", textInput: this.wTextInput, editable: this._attributeViewInfo.editable,
-                controller: this.wLabel,
+                controller: this.wLabel, step: this.step, stepDownup: this.stepDownup, stepScale: this.stepScale, minValue: this.minValue, maxValue: this.maxValue,
             }));
         }
     }
